3dc7e8cc42e08c47623bf0ab21ac08cbbf64562b
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2026-03-10 19:52:20 -0300

    finalizando UI de pagina /register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from "@angular/router";
 import { Login } from "./auth/login/login";
+import { Register } from "./auth/register/register";
 
 export const routes: Routes = [
   {
     path: '', 
     children: [
       { path: 'login', component: Login, title: "Login Page" },
-      { path: '', redirectTo: 'login', pathMatch: 'full' } 
+      { path: 'register', component: Register, title: "Login Page" },
+      // { path: '', redirectTo: 'login', pathMatch: 'full' } 
     ]
   }
 ];
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,13 +1,13 @@
-import { inject } from "@angular/core";
-import { CanActivateFn, Router } from "@angular/router";
+// import { inject } from "@angular/core";
+// import { CanActivateFn, Router } from "@angular/router";
 
-export const authGuard: CanActivateFn = (route, state) => {
-  const router = inject(Router);
-  const isAuthenticated = !!localStorage.getItem('token'); // Exemplo simples
+// export const authGuard: CanActivateFn = (route, state) => {
+//   const router = inject(Router);
+//   const isAuthenticated = !!localStorage.getItem('token'); // Exemplo simples
 
-  if (!isAuthenticated) {
-    router.navigate(['/auth/login']);
-    return false;
-  }
-  return true;
-};
+//   if (!isAuthenticated) {
+//     router.navigate(['/auth/login']);
+//     return false;
+//   }
+//   return true;
+// };
