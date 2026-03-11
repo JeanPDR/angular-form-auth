2b71ac58188f3e192809e17ca537bb8f69c01694
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2026-03-11 19:13:37 -0300

    finalizando a UI da pagina /reset-password

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from "@angular/router";
 import { Login } from "./auth/login/login";
 import { Register } from "./auth/register/register";
 import { CodeVerification } from "./auth/code-verification/code-verification";
-import { SetPassword } from "./auth/reset-password/set-password";
+import { SetPassword } from "./auth/set-password/set-password";
+import { ResetPassword } from "./auth/reset-password/reset-password";
 
 export const routes: Routes = [
   {
@@ -12,6 +13,7 @@ export const routes: Routes = [
       { path: 'register', component: Register},
       { path: 'verify', component: CodeVerification},
       { path: 'set-password', component: SetPassword},
+      { path: 'reset-password', component: ResetPassword},
       // { path: '', redirectTo: 'login', pathMatch: 'full' } 
     ]
   }
