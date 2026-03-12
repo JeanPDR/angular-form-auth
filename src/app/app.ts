483482ed81beea6f91b0173c30c368fced8ca4ba
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2026-03-12 17:03:22 -0300

    fazendo validação do cookie de sessão

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './auth/auth.service';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,4 +11,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('go-platform-auth');
+
+  private authService = inject(AuthService);
+
+  ngOnInit() {
+    // Quando o app inicia, verifica silenciosamente se o cookie ainda vale
+    this.authService.validateSession().subscribe();
+  }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environment/environment'; // Ajuste o caminho se necessário
+import { environment } from '../../environment/environment'; 
+import { catchError, map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +20,18 @@ export class AuthService {
 
   setLoggedState(status: boolean) {
     this.isLoggedIn.set(status);
+  }
+
+  validateSession() {
+    return this.http.get(`${this.apiUrl}/me`, { withCredentials: true }).pipe(
+      map(() => {
+        this.setLoggedState(true); // Token é válido!
+        return true;
+      }),
+      catchError(() => {
+        this.setLoggedState(false); // Token expirou ou não existe
+        return of(false);
+      })
+    );
   }
 }
